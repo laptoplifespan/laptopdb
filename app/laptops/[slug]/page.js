9204cc55c90dd63c1276dccff28ded9cba128e05
LaptopLifeSpan <laptoplifespan@gmail.com>
2026-06-02 19:51:16 -0400

    fix dynamic rendering for laptop slug page

--- app/laptops/[slug]/page.js
+++ app/laptops/[slug]/page.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import { supabase } from '@/lib/supabase'
 import { notFound } from 'next/navigation'
-
+export const dynamic = 'force-dynamic'
 export default async function LaptopPage({ params }) {
   const { slug } = await params
 
